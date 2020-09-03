d2bd8501286688961c12524fb7e51cf7d14b145c
spawnmarvel <espenkleivane@gmail.com>
2020-09-03 22:24:34 +0200

    read 3

--- api_rest/run_server.py
+++ api_rest/run_server.py
@@ -1,20 +1,62 @@
 # https://blog.miguelgrinberg.com/post/designing-a-restful-api-with-python-and-flask
 # https://www.flaskapi.org/
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, jsonify
 from flask_api import FlaskAPI
 # Flask API is a drop-in replacement for Flask that provides an implementation of browsable APIs similar to what Django REST framework provides. 
 # It gives you properly content negotiated-responses and smart request parsing:
 
+from flask import request, url_for
+from flask_api import FlaskAPI, status, exceptions
+
 app = FlaskAPI(__name__)
 
-@app.route('/')
-def index():
-    return {"Hello world, use route":"/api"}
 
-@app.route('/api')
-def api_main():
-    return {"You are her":"/api"}
+notes = {
+    0: "First note"
+}
 
-if __name__ == '__main__':
+def note_repr(key):
+    return {
+        'url': request.host_url.rstrip('/') + url_for('notes_detail', key=key),
+        'text': notes[key]
+    }
+
+
+@app.route("/", methods=['GET', 'POST'])
+def notes_list():
+    """
+    List or create notes.
+    """
+    if request.method == 'POST':
+        note = str(request.data.get('text', ''))
+        idx = max(notes.keys()) + 1
+        notes[idx] = note
+        return note_repr(idx), status.HTTP_201_CREATED
+
+    # request.method == 'GET'
+    return [note_repr(idx) for idx in sorted(notes.keys())]
+
+
+@app.route("/<int:key>/", methods=['GET', 'PUT', 'DELETE'])
+def notes_detail(key):
+    """
+    Retrieve, update or delete note instances.
+    """
+    if request.method == 'PUT':
+        note = str(request.data.get('text', ''))
+        notes[key] = note
+        return note_repr(key)
+
+    elif request.method == 'DELETE':
+        notes.pop(key, None)
+        return '', status.HTTP_204_NO_CONTENT
+
+    # request.method == 'GET'
+    if key not in notes:
+        raise exceptions.NotFound()
+    return note_repr(key)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
